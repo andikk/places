2a24c4ca92ae7346a3a87999d4dfb4c45e2df87b
andikk <andikk@mail.ru>
2020-08-18 12:40:39 +0300

    validation useCallback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NewPlace from "./places/pages/NewPlace";
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
 
 const App = () => {
   return (
@@ -19,6 +20,9 @@ const App = () => {
           </Route>
           <Route path="/places/new" exact>
             <NewPlace/>
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace/>
           </Route>
           <Redirect to="/" />
         </Switch>
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -25,14 +25,18 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
     const [inputState, dispatch] = useReducer(inputReducer, {
-        value: '',
-        isValid: false,
-        isTouched: false
+        value: props.value || '',
+        isTouched: false,
+        isValid: props.valid || false
     });
 
-    useEffect(()=> {
+    const {id, onInput} = props;
+    const {value, isValid} = inputState;
 
-    }, [])
+    useEffect(()=> {
+        onInput(id, value, isValid)
+    }, [id, value, isValid, onInput]);
+
 
     const changeHandler = (event) => {
         dispatch({
