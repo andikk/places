a85a9e9bcb73ea3f54d306907afec2e748ec27e8
Andrew <andikk@mail.ru>
2020-08-31 14:29:58 +0300

    File upload

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -9,6 +9,7 @@ import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import { useForm } from "../../shared/hooks/ form-hook";
 import { AuthContext } from "../../shared/context/auth-context";
 import { useHttpClient } from "../../shared/hooks/http-hook";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 
 const NewPlace = () => {
     const auth = useContext(AuthContext);
@@ -25,6 +26,10 @@ const NewPlace = () => {
         address: {
             value: '',
             isValid: false
+        },
+        image: {
+          value: null,
+          isValid: false
         }
     }, false);
 
@@ -32,18 +37,20 @@ const NewPlace = () => {
 
     const placeSubmitHandler = async (event) => {
         event.preventDefault();
+
         try {
+          const  formData = new FormData();
+          formData.append('title', formState.inputs.title.value);
+          formData.append('description', formState.inputs.description.value);
+          formData.append('address', formState.inputs.address.value);
+          formData.append('creator', auth.userId);
+          formData.append('image', formState.inputs.image.value);
+
             await sendRequest(
               'http://localhost:5000/api/places/',
               'POST',
-              JSON.stringify({
-                  title: formState.inputs.title.value,
-                  description: formState.inputs.description.value,
-                  address: formState.inputs.address.value,
-                  creator: auth.userId
-              }),
-              {'Content-Type': 'application/json'}
-              )
+              formData
+              );
             history.push('/');
         } catch (err) {
 
@@ -78,7 +85,7 @@ const NewPlace = () => {
                      validators={[VALIDATOR_REQUIRE()]}
                      errorText="Please enter a valid address"
                      onInput={inputHandler}/>
-
+              <ImageUpload id="image" onInput={inputHandler} errorText="Please provide an image"/>
               <Button type="submit" disabled={!formState.isValid}>
                   ADD PLACE
               </Button>
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -44,19 +44,20 @@ const Auth = (props) => {
 
         auth.login(responseData.user.id);
 
+
       } catch (err) {
 
       }
     } else {
       try {
+        const formData = new FormData();
+        formData.append('email', formState.inputs.email.value);
+        formData.append('name', formState.inputs.name.value);
+        formData.append('password', formState.inputs.password.value);
+        formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest('http://localhost:5000/api/users/signup',
           'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value
-          }),
-          {'Content-Type': 'application/json'}
+          formData
         );
 
         auth.login(responseData.user.id);
@@ -106,7 +107,7 @@ const Auth = (props) => {
                  errorText="Please enter a name"
                  onInput={inputHandler} />
         )}
-        {!isLoginMode && <ImageUpload center id="image" onInput={inputHandler}/>}
+        {!isLoginMode && <ImageUpload center id="image" onInput={inputHandler} errorText="Please provide an image" />}
         <Input element="input"
                id="email"
                type="email"
